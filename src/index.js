438dc6456d3dae2b8847b3d09769560930a32ebf
Jasper Steenweg <jaspersteenweg@MacBook-Pro.local>
2020-09-02 18:00:42 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,79 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+const projectFolder = "/Users/jaspersteenweg/Projects";
+
+const server = http.createServer((req, res) => {
+  console.log(req.url);
+
+  if (req.url == '/') {
+    // HOME!
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end("My Personal Webserver");
+    return;
+  }
+
+  var segments = req.url.split('/', 10);
+  var folder = segments[1];
+  var projectFolderSubpathOs = req.url.substr(`/${folder}`.length, req.url.length - `/${folder}`.length);
+  var filePathOs = `${projectFolder}/${folder}${projectFolderSubpathOs}`;
+
+  // Not found?
+  if (!fs.existsSync(filePathOs)) {
+    // 404!
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end("File not found!");
+    return;
+  }
+
+  // File exists, let's read it
+  fs.readFile(filePathOs, 'utf8', function(err, contents) {
+
+    var extension = path.extname(filePathOs);
+    var contentType = getContentTypeByExtension(extension);
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', contentType);
+    res.end(contents);
+    return;
+  });
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+
+function getContentTypeByExtension(extension) {
+  switch(extension.toLowerCase()) {
+    case ".jpg":
+    case ".jpeg": 
+      return "image/jpg";
+
+    case ".png":
+      return "image/png";
+
+    case ".gif":
+      return "image/gif";
+    
+    case ".html":
+      return "text/html";
+    
+    case ".json":
+      return "application/json";
+    
+    case ".css":
+      return "text/css";
+    
+    case ".js":
+      return "application/javascript";
+
+    default: 
+      return "text/plain";
+  }    
+}
